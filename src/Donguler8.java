b675d5ac915c49cece754254b120e5381f11e076
ugurhokelekli10 <ugurhokelekli5@gmail.com>
2023-12-14 01:43:45 +0300

    Donguler8 added

---
+++ src/Donguler8.java
@@ -0,0 +1,30 @@
+// Diamond shape with asterisks
+import java.util.Scanner;
+public class Donguler8 {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("n : ");
+        n = input.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2*i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2*i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
