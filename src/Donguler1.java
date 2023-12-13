989fc2d364fc35ac48771bf151d6762d1c24acde
ugurhokelekli10 <ugurhokelekli5@gmail.com>
2023-12-13 19:01:59 +0300

    Donguler1 updated

--- src/Donguler1.java
+++ src/Donguler1.java
@@ -1,15 +1,19 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class Donguler1 {
     public static void main(String[] args) {
+        int total = 0, count = 0;
         Scanner input = new Scanner(System.in);
         System.out.print("Bir sayı giriniz : ");
         int k = input.nextInt();
-        int total = 0;
         for (int i = 0; i <= k; i++) {
-            if (i % 3 == 0 || i % 4 == 0) total += i;
+            if (i % 3 == 0 && i % 4 == 0) {
+                total += i;
+                count++;
+            }
         }
-//      3'e ve 4'e bölünen sayıların toplamı
-        System.out.println("Total : " + total);
+        double result = (count != 0) ? (double) total / count : 0;
+
+        System.out.println("Result : " + result);
     }
 }
