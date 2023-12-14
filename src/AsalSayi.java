e5d6ba11b8cd1e6a2c358a609119095a8077197f
ugurhokelekli10 <ugurhokelekli5@gmail.com>
2023-12-14 11:54:40 +0300

    AsalSayi added

---
+++ src/AsalSayi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int n = input.nextInt();
+
+        System.out.println("Asal Sayılar: ");
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
